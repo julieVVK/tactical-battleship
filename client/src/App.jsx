0f3032b1944423874ba4a917f42f5e70c652e4f4
D0R1Sh <dorochenko2007@gmail.com>
2026-09-19 00:10:05 +0200

    Added basic frontend and backend communication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,45 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import './App.css'
 
+
 function App() {
-  return null
+
+    const [message, setMessage] = useState("");
+    const [message2, setMessage2] = useState("");
+
+    useEffect(() => {
+        fetch("/api/helloWorld")
+            .then(res => res.json())
+            .then((data) => {
+                setMessage(data.message)
+                setMessage2(data.message2)
+            })
+    }, []);
+
+    return (
+        <>
+            <div
+                style={{
+                    width: "100vw",
+                    height: "100vh",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    textAlign: "center",
+                    flexDirection: "column"
+                }}
+            >
+                <div>
+                    <h1>Hello World from frontend!</h1>
+                </div>
+                <div>
+                    <h2>And Hello World from backend:</h2>
+                    <h3> {message} </h3>
+                    <h3> {message2} </h3>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
--- server/server.js
+++ server/server.js
@@ -13,7 +13,13 @@ app.get('/', (req, res) => {
     return res.sendFile(path.join(__dirname, "server.js"))
 });
 
+app.get('/api/helloWorld', (req, res) => {
+    console.log("new api call on /api/helloWorld has been made")
+    res.json({ message: "This is Express talking to you from server.js file", message2: "Backend is working fine"})
+});
 
-app.listen(PORT, () => {
+
+app.listen(PORT, '127.0.0.1', (err) => {
+    if(err) {console.log(`ERROR has occured: ${err}`)}
     console.log(`Server has been started on http://localhost:${PORT}`)
 })
